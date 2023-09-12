6fc406f8359f22113f15c0f4dc3da041095814bb
REGSYBABA <sumimichael77@gmail.com>
2023-09-12 14:57:43 +0100

    started a little functionality thats a sidebar for adding new books

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const closeButton = document.querySelector('.close-nav')
+const openButton = document.querySelector('.open-nav')
+const nav = document.querySelector('.side-modal')
+
+closeButton.addEventListener("click", () => {
+    nav.classList.remove('navigation-open')
+})
+
+openButton.addEventListener("click", () => {
+    nav.classList.add('navigation-open')
+})
+
 console.log('hi')
+function Book(title){
+    this.title = title;
+}
+console.log(Book)
+function add(){
+    let title = "Love is Blind"
+    let newBook = new Book (title)
+    console.log(newBook)
+}
+
+add()
+let newBook = new Book ('blue')
+console.log(newBook)

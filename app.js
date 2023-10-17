1839334bc95dd0a0a81a7cfb55db0d0faf339dc0
REGSYBABA <sumimichael77@gmail.com>
2023-10-17 23:21:12 +0100

    Just figured the right way to delete accurately

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let libraryEl = document.querySelector('.book-grid')
 const closeBtn = document.querySelectorAll('.close-modal')
 const delBtn = document.getElementById('delete')
 
+
 closeNewBookButton.addEventListener("click", () => {
     nav.classList.remove('navigation-open')
 })
@@ -16,12 +17,14 @@ addNewBookButton.addEventListener("click", () => {
 })
 
 
-const showModal = (openButton, modalContent) => {
+const showModal = (openButton, modalContent, bookIndex) => {
     const openBtn = document.getElementById(openButton),
         modalContainer = document.getElementById(modalContent)
 
     if (openBtn && modalContainer) {
         modalContainer.classList.add('show-modal')
+        document.getElementById('delete').setAttribute('data-index', bookIndex);
+
     }
 }
 
@@ -33,14 +36,15 @@ function closeModal() {
 closeBtn.forEach(closebutton => closebutton.addEventListener('click', closeModal))
 
 
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, pages, language, read) {
+function Book(title, author, pages, language, read, id) {
     this.title = title
     this.author = author
     this.pages = pages
     this.language = language
     this.read = read
+    this.id = id
 }
 
 Book.prototype.toggleRead = function () {
@@ -53,20 +57,23 @@ function toggleRead(index) {
 }
 
 function render() {
+
     let libraryEl = document.querySelector('.book-grid')
+
     libraryEl.innerHTML = ""
     for (let i = 0; i < myLibrary.length; i++) {
         let books = myLibrary[i]
         let booksEl = document.createElement("div")
-        modalEl = document.getElementById('delete-modal')
+        booksEl.setAttribute('data-number', books.id)
+        booksEl.classList.add('love')
         booksEl.innerHTML =
             `<div class = "book-card ${books.read ? "read" : " "}">
                 <div class="del-icon">
-                <i class="material-icons mat" id="open-modal" onclick="showModal('open-modal', 'modal-container')", onclick="removeBook(${i})">delete</i>
+                <i class="material-icons mat" id="open-modal" onclick="showModal('open-modal', 'modal-container', ${books.id})">delete</i>
                 </div>
 
                 <div class="content">
-                <h2>Title: ${books.title}</h2>
+                <h2 onclick="removeBook(${books.id})">Title: ${books.title}</h2>
                 <h2>Author: ${books.author}</h2>
                 <h2>Pages: ${books.pages}</h2>
                 <h2>Language: ${books.language}</h2>
@@ -79,34 +86,16 @@ function render() {
                 </div>
                 </div>
             </div>`
-
-        modalEl.innerHTML = ` <div class="modal__container" id="modal-container">
-        <div class="modal-content">
-          <div class="modal__close" title="Close">
-            <i class="material-icons close-modal" onclick="closeModal()">close</i>
-          </div>
-
-          <img src="img/thrash.png" alt="thrash" class="modal__img" />
-
-          <h4 class="modal__title">
-            Are You Sure You Want to delete this book?
-          </h4>
-          <p class="modal__description">once done cannot be undone</p>
-
-          <button class="modal__button modal__button-width" id="delete" onclick="removeBook(${i})">
-            Very Sure
-          </button>
-          <button class="modal__button-link close-modal" onclick="closeModal()">Go Back</button>
-        </div>
-      </div>`
         libraryEl.appendChild(booksEl)
         // onclick="showModal('open-modal', 'modal-container')" onclick="removeBook(${i})
     }
 }
 
-function removeBook(index) {
-    myLibrary.splice(index, 1)
-    render()
+function deleteBook() {
+    const bookIndex = document.getElementById('delete').getAttribute('data-index');
+    myLibrary.splice(bookIndex, 1);
+    closeModal();
+    render();
 }
 
 function addBookToLibrary() {
@@ -115,7 +104,8 @@ function addBookToLibrary() {
     let pages = document.querySelector('#Pages').value
     let language = document.querySelector('#lang').value
     let read = document.querySelector('#is-read').checked
-    let newBook = new Book(title, author, pages, language, read)
+    let id = myLibrary.length
+    let newBook = new Book(title, author, pages, language, read, id)
     myLibrary.push(newBook)
     console.log(myLibrary)
     render()
@@ -126,9 +116,7 @@ submitBtn.addEventListener('click', function (e) {
     addBookToLibrary()
 })
 
-// delBtn.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     removeBook()
-//     render()
-//     closeModal()
-// })
+delBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteBook();
+})

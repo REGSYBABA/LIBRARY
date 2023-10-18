0f315afa7ab260865aae222244b3634b0bc0c101
REGSYBABA <sumimichael77@gmail.com>
2023-10-18 10:24:09 +0100

    Worked on my Validate Input Function for security reasons

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const body = document.querySelector('body')
 const closeNewBookButton = document.querySelector('.close-nav')
 const addNewBookButton = document.querySelector('.open-nav')
 const nav = document.querySelector('.side-modal')
@@ -6,8 +5,16 @@ const submitBtn = document.querySelector('.submit-newbook')
 let libraryEl = document.querySelector('.book-grid')
 const closeBtn = document.querySelectorAll('.close-modal')
 const delBtn = document.getElementById('delete')
+const form = document.getElementById('my-form')
+const bookTitle = document.getElementById('book-title')
+const author = document.getElementById('author')
+const numOfPages = document.getElementById('Pages')
+const language = document.getElementById('lang')
+const password = document.getElementById('password')
+const password2 = document.getElementById('confirmPassword')
+const btn = document.querySelector('button')
 
-
+//Event Listeners
 closeNewBookButton.addEventListener("click", () => {
     nav.classList.remove('navigation-open')
 })
@@ -16,6 +23,97 @@ addNewBookButton.addEventListener("click", () => {
     nav.classList.add('navigation-open')
 })
 
+closeBtn.forEach(closebutton => closebutton.addEventListener('click', closeModal))
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validateInputs()
+})
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (validateInputs()) {
+        addBookToLibrary();
+    }
+})
+
+delBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteBook();
+})
+
+
+// Library array
+let myLibrary = [];
+
+//classes
+class Book {
+    constructor (title, author, pages, language, read, id) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.language = language
+        this.read = read
+        this.id = id
+    }
+    toggleRead() {
+        this.read = !this.read
+    }
+}
+
+//Functions
+function addBookToLibrary() {
+    let title = document.querySelector('#book-title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#Pages').value
+    let language = document.querySelector('#lang').value
+    let read = document.querySelector('#is-read').checked
+    let id = myLibrary.length
+    let newBook = new Book(title, author, pages, language, read, id)
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+    render()
+}
+
+function render() {
+    let libraryEl = document.querySelector('.book-grid');
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let books = myLibrary[i];
+        let booksEl = createBookElement(books, i);
+        libraryEl.appendChild(booksEl);
+    }
+}
+
+function createBookElement(book, index) {
+    let booksEl = document.createElement("div");
+    booksEl.setAttribute('data-number', book.id);
+    booksEl.classList.add('love');
+    booksEl.innerHTML = `
+        <div class="book-card ${book.read ? "read" : ""}">
+            <div class="del-icon">
+            <i class="material-icons mat" id="open-modal" onclick="showModal('open-modal', 'modal-container', ${book.id})">delete</i>
+            </div>
+            <div class="content">
+                <h2>Title: ${book.title}</h2>
+                <h2>Author: ${book.author}</h2>
+                <h2>Pages: ${book.pages}</h2>
+                <h2>Language: ${book.language}</h2>
+            </div>
+            <div class="toggle-switch ${book.read ? "read" : ""}" onclick="toggleRead(${index})">
+                <span class="controller-text">${book.read ? "Read:" : "Not Read:"}</span>
+                <div class="switch">
+                    <span class="controller"></span>
+                </div>
+            </div>
+        </div>`;
+    return booksEl;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead()
+    render()
+}
 
 const showModal = (openButton, modalContent, bookIndex) => {
     const openBtn = document.getElementById(openButton),
@@ -33,62 +131,66 @@ function closeModal() {
     modalContainer.classList.remove('show-modal')
 }
 
-closeBtn.forEach(closebutton => closebutton.addEventListener('click', closeModal))
+const setError = (element, message) => {
+    const inputs = element.parentElement
+    const errorDisplay = inputs.querySelector('.error')
 
+    errorDisplay.textContent = message
+    inputs.classList.add('error')
+    inputs.classList.remove('success')
 
-let myLibrary = [];
-
-function Book(title, author, pages, language, read, id) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.language = language
-    this.read = read
-    this.id = id
 }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read
+const setSuccess = (element) => {
+    const inputs = element.parentElement
+    const errorDisplay = inputs.querySelector('.error')
+
+    errorDisplay.innerText = ''
+    inputs.classList.add('success')
+    inputs.classList.remove('error')
 }
 
-function toggleRead(index) {
-    myLibrary[index].toggleRead()
-    render()
-}
+const validateInputs = () => {
+    const bookTitleVal = bookTitle.value.trim()
+    const authorVal = author.value.trim()
+    const numOfPagesVal = numOfPages.value.trim()
+    const languageVal = language.value.trim()
 
-function render() {
-
-    let libraryEl = document.querySelector('.book-grid')
-
-    libraryEl.innerHTML = ""
-    for (let i = 0; i < myLibrary.length; i++) {
-        let books = myLibrary[i]
-        let booksEl = document.createElement("div")
-        booksEl.setAttribute('data-number', books.id)
-        booksEl.classList.add('love')
-        booksEl.innerHTML =
-            `<div class = "book-card ${books.read ? "read" : " "}">
-                <div class="del-icon">
-                <i class="material-icons mat" id="open-modal" onclick="showModal('open-modal', 'modal-container', ${books.id})">delete</i>
-                </div>
-
-                <div class="content">
-                <h2 onclick="removeBook(${books.id})">Title: ${books.title}</h2>
-                <h2>Author: ${books.author}</h2>
-                <h2>Pages: ${books.pages}</h2>
-                <h2>Language: ${books.language}</h2>
-                </div>
-
-                <div class="toggle-switch ${books.read ? "read" : " "}" onclick="toggleRead(${i})">
-                <span class="controller-text">${books.read ? "Read:" : "Not Read:"}</span>
-                <div class="switch">
-                    <span class="controller"></span>
-                </div>
-                </div>
-            </div>`
-        libraryEl.appendChild(booksEl)
-        // onclick="showModal('open-modal', 'modal-container')" onclick="removeBook(${i})
+    if (bookTitleVal === '') {
+        setError(bookTitle, "Book's name is required")
+        return false;
+    } else if (bookTitleVal.length < 4) {
+        setError(bookTitle, "Book's name should be more than 4")
+        return false;
+    } else if (/\d/.test(bookTitleVal)) {
+        setError(bookTitle, "Book's name can't contain a number");
+        return false;
+    } else {
+        setSuccess(bookTitle)
     }
+
+    if (authorVal === '') {
+        setError(author, 'Last name is required')
+        return false;
+    } else {
+        setSuccess(author)
+    }
+
+    if (numOfPagesVal === '') {
+        setError(numOfPages, 'Number of pages is required')
+        return false;
+    } else {
+        setSuccess(numOfPages)
+    }
+
+    if (languageVal === '') {
+        setError(language, 'Language is Required')
+        return false;
+    } else {
+        setSuccess(language)
+    }
+
+    return true;
 }
 
 function deleteBook() {
@@ -97,26 +199,3 @@ function deleteBook() {
     closeModal();
     render();
 }
-
-function addBookToLibrary() {
-    let title = document.querySelector('#book-title').value
-    let author = document.querySelector('#author').value
-    let pages = document.querySelector('#Pages').value
-    let language = document.querySelector('#lang').value
-    let read = document.querySelector('#is-read').checked
-    let id = myLibrary.length
-    let newBook = new Book(title, author, pages, language, read, id)
-    myLibrary.push(newBook)
-    console.log(myLibrary)
-    render()
-}
-
-submitBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    addBookToLibrary()
-})
-
-delBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    deleteBook();
-})
